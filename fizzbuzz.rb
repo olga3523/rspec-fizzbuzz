1fc5781e1e544b64c5666b577e80c379accdf40c
OLGA <olichka23.85@gmail.com>
2021-09-14 10:46:33 -0400

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+# See README.md for instructions on how to do thi
+
+def fizzbuzz( num )
+    if( num == 3 )
+        return "Fizz";
+    elsif( num == 4 )
+        return nil
+    elsif( num == 5 )
+        return "Buzz"
+    elsif( num == 15 )
+        return "FizzBuzz"
+    end
+end
